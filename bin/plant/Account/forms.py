88a76420347f29d284c97106b2c6eb8cde1b1892
ayda@ayda <ayda.f.naderi@gmail.com>
2020-10-19 18:05:06 +0330

    edit reset password

--- bin/plant/Account/forms.py
+++ bin/plant/Account/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     username = forms.IntegerField(min_value = 9000000000 ,max_value = 9999999999)
@@ -10,13 +9,7 @@ class SignUpForm(UserCreationForm):
     password2 = forms.CharField(max_length = 50,validators = [alphanumeric],widget = forms.PasswordInput())
     email = forms.EmailField(max_length = 254)
 
-class ResetForm(forms.ModelForm):
+class ResetForm(forms.Form):
     alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
-    password1 = forms.CharField(max_length = 50, validators = [alphanumeric], widget = forms.PasswordInput())
-    password2 = forms.CharField(max_length = 50, validators = [alphanumeric], widget = forms.PasswordInput())
-    class Meta:
-        model = User
-        fields = ('username','password','email',)
-        widgets = {
-            'password': forms.PasswordInput(),
-        }
+    password1 = forms.CharField(min_length = 8,max_length = 50, validators = [alphanumeric], widget = forms.PasswordInput())
+    password2 = forms.CharField(min_length = 8,max_length = 50, validators = [alphanumeric], widget = forms.PasswordInput())
