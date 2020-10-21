a9edca4b186fc0f03bd75706d6410cf8be578460
ayda@ayda <ayda.f.naderi@gmail.com>
2020-10-21 17:08:09 +0330

    setting app

--- bin/plant/plant/settings.py
+++ bin/plant/plant/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     "Account.apps.AccountConfig",
+    "Home.apps.HomeConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
