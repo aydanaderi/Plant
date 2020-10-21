0bd9d78ea87cf67f35f184773d3478379402aaf8
ayda@ayda <ayda.f.naderi@gmail.com>
2020-10-21 16:51:04 +0330

    create data base

--- bin/plant/Home/models.py
+++ bin/plant/Home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model) :
+    name = models.CharField()
+    English_name = models.CharField()
+    irrigation_quantity = models.CharField()
+    irrigation = models.CharField()
+    temprature_quantity = models.CharField()
+    temprature = models.TextField()
+    light_quantity = models.CharField()
+    light = models.TextField()
+    humidity_quantity = models.CharField()
+    humidity = models.TextField()
+    soil = models.TextField()
+    fertilizer = models.TextField()
+    problems = models.TextField()
+    potting = models.TextField()
+    poisonous = models.CharField()
+    sensibility = models.CharField()
+
+
+
