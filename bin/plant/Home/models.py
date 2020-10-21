c302acc4788811af0e817ab594e7eb47621a50f9
ayda@ayda <ayda.f.naderi@gmail.com>
2020-10-21 18:27:18 +0330

    edit data base

--- bin/plant/Home/models.py
+++ bin/plant/Home/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 
 class Plant(models.Model) :
-    name = models.CharField()
-    English_name = models.CharField()
-    irrigation_quantity = models.CharField()
-    irrigation = models.CharField()
-    temprature_quantity = models.CharField()
+    amount = (
+        ('l' , 'low'),
+        ('m' , 'middle'),
+        ('h' , 'high'),
+    )
+    answer = (
+        ('y' , 'yes'),
+        ('n' , 'no'),
+    )
+    name = models.CharField(max_length = 500)
+    English_name = models.CharField(max_length = 500)
+    irrigation_quantity = models.CharField(max_length = 1,choices = amount)
+    irrigation = models.CharField(max_length = 500)
+    temprature_quantity = models.CharField(max_length = 500)
     temprature = models.TextField()
-    light_quantity = models.CharField()
+    light_quantity = models.CharField(max_length = 1,choices = amount)
     light = models.TextField()
-    humidity_quantity = models.CharField()
+    humidity_quantity = models.CharField(max_length = 1,choices = amount)
     humidity = models.TextField()
     soil = models.TextField()
     fertilizer = models.TextField()
     problems = models.TextField()
     potting = models.TextField()
-    poisonous = models.CharField()
-    sensibility = models.CharField()
+    poisonous = models.CharField(max_length = 1,choices = answer)
+    sensibility = models.CharField(max_length = 1,choices = amount)
 
 
 
