4dc931d8bf9f14897e10a4991d51ea609f8a0c4a
ayda@ayda <ayda.f.naderi@gmail.com>
2020-10-22 17:55:55 +0330

    edit

--- bin/plant/Home/models.py
+++ bin/plant/Home/models.py
@@ -2,30 +2,35 @@ from django.db import models
 
 class Plant(models.Model) :
     amount = (
-        ('l' , 'low'),
-        ('m' , 'middle'),
-        ('h' , 'high'),
+        ('low' , 'low'),
+        ('middle' , 'middle'),
+        ('high' , 'high'),
+    )
+    quantity = (
+        ('low', 'low'),
+        ('middle', 'middle'),
+        ('high', 'high'),
     )
     answer = (
-        ('y' , 'yes'),
-        ('n' , 'no'),
+        ('yes' , 'yes'),
+        ('no' , 'no'),
     )
     name = models.CharField(max_length = 500 , default = '')
     English_name = models.CharField(max_length = 500,default = '')
-    irrigation_quantity = models.CharField(max_length = 1,choices = amount,default = '')
+    irrigation_quantity = models.CharField(max_length = 10,choices = amount,default = '')
     irrigation = models.CharField(max_length = 10000,default = '')
     temprature_quantity = models.CharField(max_length = 500,default = '')
     temprature = models.CharField(max_length = 10000,default = '')
-    light_quantity = models.CharField(max_length = 1,choices = amount,default = '')
+    light_quantity = models.CharField(max_length = 10,choices = amount,default = '')
     light = models.CharField(max_length = 10000,default = '')
-    humidity_quantity = models.CharField(max_length = 1,choices = amount,default = '')
+    humidity_quantity = models.CharField(max_length = 10,choices = amount,default = '')
     humidity = models.CharField(max_length = 10000,default = '')
     soil = models.CharField(max_length = 10000,default = '')
     fertilizer = models.CharField(max_length = 10000,default = '')
     problems = models.CharField(max_length = 10000,default = '')
     potting = models.CharField(max_length = 10000,default = '')
-    poisonous = models.CharField(max_length = 1,choices = answer,default = '')
-    sensibility = models.CharField(max_length = 1,choices = amount,default = '')
+    poisonous = models.CharField(max_length = 5,choices = answer,default = '')
+    sensibility = models.CharField(max_length = 10,choices = amount,default = '')
 
 
 
--- bin/plant/Home/views.py
+++ bin/plant/Home/views.py
@@ -1,1 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
+from . import models
+
+def SearchView(request):
+    return  HttpResponse('hi')
