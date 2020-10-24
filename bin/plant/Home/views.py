165d7cc9ed8d617675362877a7d8a7ab96f9de65
ayda@ayda <ayda.f.naderi@gmail.com>
2020-10-24 19:43:12 +0330

    data base

--- bin/plant/Home/views.py
+++ bin/plant/Home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from difflib import SequenceMatcher
+from django.views import generic
 from . import models
 
 def HomeView(request):
@@ -10,13 +11,21 @@ def SearchView(request):
     if request.method == "POST":
         name = request.POST['search']
         list = []
+        max = 0.6
+        db = ''
         for l in models.Plant.objects.all():
             ratio1 = SequenceMatcher(None, l.name, name).ratio()
             if ratio1 > 0.6 :
-                list.append(l.name)
-        if len(list) == 0 :
+                if ratio1 == 1:
+                    db = l.name
+                    break
+                if ratio1 > max:
+                    max = ratio1
+                    db = l.name
+        if db == '' :
             return render(request, 'search.html', {'error': 'یافت نشد'})
         else :
-            return render(request,'plant.html',{'list' : list})
+            plnt = models.Plant.objects.all()
+            return render(request, 'plant.html', {'db' : db,'plnt' : plnt})
     else:
         return render(request, 'search.html')
