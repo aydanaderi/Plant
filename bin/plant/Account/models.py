34143ed826c2a5ee55e7799968817c570ae3530f
ayda@ayda <ayda.f.naderi@gmail.com>
2020-11-05 17:35:33 +0330

    edit

--- bin/plant/Account/models.py
+++ bin/plant/Account/models.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 from django.db import models
-from PIL import Image
 
 class Information(models.Model) :
     id = models.AutoField(primary_key=True)
@@ -9,13 +8,6 @@ class Information(models.Model) :
     email = models.EmailField(max_length = 254)
     date = models.DateTimeField()
     profile = models.ImageField(default = 'pic.jpg')
+
     def __str__(self):
         return str(self.username)
-
-"""    def save(self, *args , **kwargs):
-        super().save(*args , **kwargs)
-        img = Image.open(self.profile.path)
-        if img.height > 100 or img.weight > 100 :
-            output_size = (100,100)
-            img.thumbnail(output_size)
-            img.save(self.profile.path)"""
